b4469c24c5b87cb2b3d230844f94166c5ec1a9ba
Ricardo <ricardoado27@gmail.com>
2025-02-21 12:33:38 +0700

    update sayhello dgn parameter, jatuhnya major changes krn ngerusak code sblmny

--- go.mod
+++ go.mod
@@ -1,3 +1,6 @@
-module github.com/Ric255-r/golang-say-hello
+module github.com/Ric255-r/golang-say-hello/v2
+// module github.com/Ric255-r/golang-say-hello
 
 go 1.24.0
+
+// namun jika g bs dihindari, strategi terbaik adalah merubah nama module, biasanya nambah slash /v2 atau apa gt
--- say_hello.go
+++ say_hello.go
@@ -1,5 +1,16 @@
 package golangsayhello
 
-func SayHello() string {
-	return "Hai Dunia"
+func SayHello(name string) string {
+	return "Hai" + name
 }
+
+// cmd git baru, setelah kita push ke repo, kita bs rilis tag versi
+// git tag v1.0.0
+// git push origin v1.0.0
+
+// disarankan klo kita udah push, ganti versi tagnya walaupun minor, supaya perubahan kesimpan
+// misalkan v1.0.1
+
+// note jika ada major upgrade seperti perubahan pada isi program kita, sehingga membuatnya tidak backward compatible
+// dimana haal ini sebisa mungkin dihindari
+// namun jika g bs dihindari, strategi terbaik adalah merubah nama module
